d9d9f812f0d36cf81e5964e37a6e29934c4551fc
Gina W <watanabegina26@gmail.com>
2018-04-07 15:13:54 -1000

    Loaded test mesh to scene.

--- app.js
+++ app.js
@@ -11,12 +11,19 @@ window.onload = function() {
     container = document.createElement('div');
     document.body.appendChild(container);
     scene = new THREE.Scene();
-    scene.add(mesh);
 
     //Initialize camera.
     camera = new THREE.PerspectiveCamera(70, width / height, 1, 500);
     camera.position.set(0, 3.5, 10);
     camera.lookAt(scene.position);
+
+    //Add test mesh.
+    let loader = new THREE.JSONLoader();
+    let name = 'models/testbrain.json';
+    loader.load(name, function(geometry, materials) {
+      mesh = new THREE.Mesh(geometry, new THREE.MeshFaceMaterial(materials));
+      scene.add(mesh);
+    });
   }
 
 }
