eea49c330346731c46ab9971057fabdbdd86295a
Gina W <watanabegina26@gmail.com>
2018-04-07 15:11:42 -1000

    Initialized mesh variable and camera.

--- app.js
+++ app.js
@@ -4,6 +4,19 @@ window.onload = function() {
   let container, scene, camera, renderer, light;
   let raycaster = new THREE.Raycaster();
   let mouse = new THREE.Vector2(), INTERSECTED, CLICKINTERSECTED;
-  let intersects; 
+  let intersects;
+  let mesh = null;
+
+  function init() {
+    container = document.createElement('div');
+    document.body.appendChild(container);
+    scene = new THREE.Scene();
+    scene.add(mesh);
+
+    //Initialize camera.
+    camera = new THREE.PerspectiveCamera(70, width / height, 1, 500);
+    camera.position.set(0, 3.5, 10);
+    camera.lookAt(scene.position);
+  }
 
 }
