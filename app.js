27c1e0d14b2667af6b1555e5665acb87845e76bc
Gina W <watanabegina26@gmail.com>
2018-04-07 15:09:43 -1000

    Initialized variables.

---
+++ app.js
@@ -0,0 +1,9 @@
+window.onload = function() {
+  let width = window.innerWidth;
+  let height = window.innerHeight;
+  let container, scene, camera, renderer, light;
+  let raycaster = new THREE.Raycaster();
+  let mouse = new THREE.Vector2(), INTERSECTED, CLICKINTERSECTED;
+  let intersects; 
+
+}
